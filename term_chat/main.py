7ed91ca5d0d291591823848dff62ead8cbba1a12
junasliang <jun942670@gmail.com>
2025-08-15 17:02:51 +0800

    feat: chatbot with memory/summary

--- term_chat/main.py
+++ term_chat/main.py
@@ -2,25 +2,38 @@
 # How to maintain chat model
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+
+from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory, FileChatMessageHistory
 
 from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
 
-    chat = ChatOpenAI()
-
+    # set up chat
+    chat = ChatOpenAI(verbose=True)
+    # set up memory
+    memory = ConversationSummaryBufferMemory(
+        # chat_memory=FileChatMessageHistory("messages.json"),
+        memory_key = "messages",
+        return_messages = True,   # return object instead of text
+        llm=chat,
+    )
+    
     prompt = ChatPromptTemplate(
-        input_variables = ["content"],
+        input_variables = ["content", "messages"],
         messages=[
+            MessagesPlaceholder(variable_name="messages"),
             HumanMessagePromptTemplate.from_template("{content}")
         ]
     )
 
     chain = LLMChain(
         llm = chat,
-        prompt = prompt
+        prompt = prompt,
+        memory = memory,
+        verbose = True,
     )
 
     while True:
