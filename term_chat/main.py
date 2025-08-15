940ace7005818a32401e8fcffbcc2d0669650f1a
junasliang <jun942670@gmail.com>
2025-08-15 16:27:09 +0800

    feat: no memory chatbot

--- term_chat/main.py
+++ term_chat/main.py
@@ -1,7 +1,30 @@
+# Most of the models are completion model
+# How to maintain chat model
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
 
-
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    chat = ChatOpenAI()
+
+    prompt = ChatPromptTemplate(
+        input_variables = ["content"],
+        messages=[
+            HumanMessagePromptTemplate.from_template("{content}")
+        ]
+    )
+
+    chain = LLMChain(
+        llm = chat,
+        prompt = prompt
+    )
+
     while True:
         content = input(">>")
-        print(f"You enterd: {content}")
+        result = chain({"content": content})
+
+        print(result["text"])
