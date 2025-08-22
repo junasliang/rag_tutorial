54ad91d2e63a71016a2ae6d99b77cfbd1a0ca13d
梁凱鈞 <liangkaijun@liangkaijundeMac-mini.local>
2025-08-22 21:13:12 +0800

    feat: apply custom filter

--- fact_chat/prompt.py
+++ fact_chat/prompt.py
@@ -3,7 +3,10 @@ from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from redundant_filter_retriever import RedundantFilterRetriever
+import langchain
 
+langchain.debug = True
 load_dotenv()
 
 chat = ChatOpenAI()
@@ -11,7 +14,10 @@ embeddings = OpenAIEmbeddings()
 # create embeddings db
 db = Chroma(persist_directory="emb", embedding_function=embeddings)
 
-retriever = db.as_retriever()
+retriever = RedundantFilterRetriever(
+    embeddings=embeddings,
+    chroma=db,
+)
 
 chain = RetrievalQA.from_chain_type(llm=chat, retriever=retriever, chain_type="stuff")
 
