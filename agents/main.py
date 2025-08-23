28df5870a030abeb5de776ecdc6dbdd94b8c9e3f
梁凱鈞 <liangkaijun@liangkaijundeMac-mini.local>
2025-08-23 22:33:13 +0800

    feat: add sql query tool

--- agents/main.py
+++ agents/main.py
@@ -5,24 +5,34 @@ from langchain.prompts import(
     MessagesPlaceholder,
 )
 
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
+
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite database.\n"
+            f"The databse has tables of: {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead, use the 'describe_tables' function."
+        )),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad") #simplified form of memory
     ]
 )
 
 # define tools
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 # define agent 
 # (chain that uses tools)
@@ -40,4 +50,5 @@ agent_executor = AgentExecutor(
     tools=tools,
 )
 
-agent_executor("How many users are in the database?")
+agent_executor("How many users have provided a shipping address?")
+#agent_executor("How many users are in the database?")
