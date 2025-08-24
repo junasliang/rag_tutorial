8bf5677e5c3283943e9e423badb9a2e44f4022bb
梁凱鈞 <liangkaijun@liangkaijundeMac-mini.local>
2025-08-24 21:17:57 +0800

    feat: add report html tool

--- agents/tools/sql.py
+++ agents/tools/sql.py
@@ -1,4 +1,6 @@
 import sqlite3
+from pydantic.v1 import BaseModel
+from typing import List
 from langchain.tools import Tool
 
 # connect to sql database
@@ -18,12 +20,17 @@ def list_tables():
     c.execute("SELECT name FROM sqlite_master WHERE type='table';")
     rows = c.fetchall()
     return "\n".join(row[0] for row in rows if row[0] is not None)
-    
+
+
+class RunQueryArgsSchema(BaseModel):
+    query: str
+
 # create langchain tool
 run_query_tool = Tool.from_function(
     name="run_squlite_query",
     description="Run a sqlite query.",
     func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema
 )
 
 def describe_tables(tables_names):
@@ -32,8 +39,12 @@ def describe_tables(tables_names):
     rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name IN ({tables});")
     return '\n'.join(row[0] for row in rows if row[0] is not None)
 
+class DescribeTablesArgsSchema(BaseModel):
+    tables_names: List[str]
+
 describe_tables_tool = Tool.from_function(
     name="describe_tables",
     description="Given a list of table names, returns the schema of those tables",
     func=describe_tables,
+    args_schema=DescribeTablesArgsSchema
 )
